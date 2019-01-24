80ba81a1cc6879d62407df9dfca2b71ff450b52f
Humza <usman@sheridancollege.ca>
2019-01-24 17:05:03 -0500

    first commit

--- src/pillbox/Student.java
+++ src/pillbox/Student.java
@@ -13,6 +13,24 @@ public class Student {
     
     public Student(){}
     
+    private String name;
+    private int studentID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
     
     
     
